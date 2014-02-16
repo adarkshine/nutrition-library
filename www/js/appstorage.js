2161f555eb894b5e710332cc77b4b865438382ed
adarkshine <hedgeh0gman@aol.com>
2014-02-16 19:09:34 +0000

    Update 0.10

--- www/js/appstorage.js
+++ www/js/appstorage.js
@@ -1,11 +1,11 @@
-var i = localStorage.length;
+var i = window.localStorage.length;
 function onBodyLoad(){
 	var todo = "";
 	
 	create_new_list();
 	
 	$("#clear").click(function(){
-		localStorage.clear();
+		window.localStorage.clear();
 		$("#todo_list li").fadeOut(function(){
 			$(this).html("");
 		});
@@ -16,13 +16,13 @@ function onBodyLoad(){
 		$(this).closest("li").slideUp(function(){
 		
 			// remove the selected item
-			localStorage.removeItem('names_'+index);
+			window.localStorage.removeItem('names_'+index);
 			
 			// rearrange localstorage array 
-			for(i=0; i<localStorage.length; i++) {
-			  if( !localStorage.getItem("names_"+i)) {
-				localStorage.setItem("names_"+i, localStorage.getItem('names_' + (i+1) ) );
-				localStorage.removeItem('names_'+ (i+1) );
+			for(i=0; i<window.localStorage.length; i++) {
+			  if( !window.localStorage.getItem("names_"+i)) {
+				window.localStorage.setItem("names_"+i, window.localStorage.getItem('names_' + (i+1) ) );
+				window.localStorage.removeItem('names_'+ (i+1) );
 			  }
 			}
 			
@@ -35,8 +35,8 @@ function onBodyLoad(){
 	});
 }
 function create_new_list(){
-	for (var i = 0; i < localStorage.length; i++){
-		todo = localStorage.getItem('names_'+i);
+	for (var i = 0; i < window.localStorage.length; i++){
+		todo = window.localStorage.getItem('names_'+i);
 		$("#todo_list").append('<li id="'+i+'"><a href="#">'+todo+'</a><a href="#" data-rel="dialog" data-transition="slideup" id="remove">Remove</a></li>');
 	}	
 	// Refresh list so jquery mobile can apply iphone look to the list
@@ -47,7 +47,7 @@ function save_todo(){
 	var todo = $("#textinput1").val();
 	if(todo.length){
 		// store item in local storage
-		localStorage['names_'+i] = todo;
+		window.localStorage['names_'+i] = todo;
 
 		// Update todo list
 		$("#todo_list").append('<li id="'+i+'"><a href="#">'+todo+'</a><a href="#" data-rel="dialog" data-transition="slideup" id="remove">Remove</a></li>');
